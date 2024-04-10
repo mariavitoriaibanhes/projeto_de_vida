c323af136cb76e43c2a94477660b26e49d4c28d5
mariavitoriaibanhes <130059303+mariavitoriaibanhes@users.noreply.github.com>
2024-04-10 12:31:13 -0300

    s

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const botoes = document.querySelectorAll(".botao");
 
 for(let i=0;i <botoes.length;i++){
-    console.log(i);
+    botoes[i].onclick = function(){
+        botoes[i].classList.add(
+            
+        )
+    }
     
+
 }
