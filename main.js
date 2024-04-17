c8fcc0b7acc312fe0004c0e12cf9b40f4dfd53e4
mariavitoriaibanhes <130059303+mariavitoriaibanhes@users.noreply.github.com>
2024-04-17 12:14:41 -0300

    s

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const botoes = document.querySelectorAll(".botao");
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
-        botoes[i].classList.add(
-            
-        )
-    }
-    
 
+        for(let j=0;j<botoes.length;jj++){
+            botoes[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo")
+    }
+    console.log();
 }
