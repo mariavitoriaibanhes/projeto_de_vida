2ea22035f5a15be144144e245b4ac681d34ce18e
mariavitoriaibanhes <130059303+mariavitoriaibanhes@users.noreply.github.com>
2024-04-29 10:54:31 -0300

    s

--- main.js
+++ main.js
@@ -18,7 +18,20 @@ for(let i=0;i <botoes.length;i++){
     const tempoObjetivo1 = new Date(“2023-11-05T00:00:00”);//adicionado
     const tempoObjetivo1 = new Date(“2023-12-30T00:00:00”);//adicionado
 
-    const tempos = [tempoObjetivo1,tempoObjetivo2;tempoObjetivo3,tempoObjetivo4];
+    const tempos = [tempoObjetivo1,tempoObjetivo2;tempoObjetivo3,tempoObjetivo4];//adicionado
 
     function calculaTempo(tempoObjetivo) {
         let tempoAtual = new Date();
+        let tempoFinal = tempoObjetivo - tempoAtual;
+        let segundos = Math.floor(tempoFinal / 1000);
+        let minutos = Math.floor(segundos / 60);
+        let horas = Math.floor(minutos / 60);
+        let dias = Math.floor(horas / 24);
+
+        segundos %= 60;
+        minutos %= 60;
+        horas %= 24;
+
+        if(tempoFinal > 0){
+        return dias + “ dias “ + horas + “ horas “ + minutos + “minutos “ + segundos + “ segundos”;
+}
